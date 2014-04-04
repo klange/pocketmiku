1956ca9b767ff5e086642f5cc4764cdffd7b0bf3
Kevin Lange <kevin.lange@dakko.us>
2014-04-03 21:32:10 -0700

    another demo

---
+++ test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import time
+
+from amidi import send
+from control import noteOn, noteOff, notesToMidiString
+from notes import characters
+
+scale = [60, 62, 64, 65, 67, 69, 71, 72]
+names = ['C','D','E','F','G','A','B','C']
+
+for i in xrange(0, 0x80):
+	print "Playing a %s on a %s." % (characters[i], names[i % len(names)])
+	send(notesToMidiString([i]))
+	send(noteOn(scale[i % len(names)]))
+	time.sleep(0.5)
+	send(noteOff(scale[i % len(names)]))
